057473824a837ef90909183bb3f78bc2ad2c7993
Yekaterina Shabunin <katka2004@hotmail.com>
2024-11-15 14:18:40 -0500

    init commit

---
+++ src/Component/EmployeeForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../EmployeeForm.css';
+
+class EmployeeForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name:'', email: '', title:'', department:''};
+    
+    }
+// methods for handling input and form submission
+handleSubmit= (e)=>{
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+        name:'', email: '', title:'', department:''
+    });
+};
+handleChange= (e)=>{
+    const {name,value} = e.target;
+    this.setState({
+        [name]: value
+    });
+};
+render(){
+    return (
+        <form className="EmployeeForm" onSubmit={this.handleSubmit}>
+            <h1>Add Employee</h1>
+            <div>
+                <label htmlFor="name"> Name: </label>
+                <input type= "text" name= "name" id= "name" onChange={this.handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="email"> Email: </label>
+                <input type= "text" name= "email" id= "email" onChange={this.handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="title" id="lbltitle"> Title: </label>
+                <input type= "text" name= "title" id= "title" onChange={this.handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="department"> Department: </label>
+                <input type= "text" name= "department" id= "department" onChange={this.handleChange}/>
+            </div>
+            <button type= "submit">Add</button>
+        </form>
+    );
+}
+}
+export default EmployeeForm;
