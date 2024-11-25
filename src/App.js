c35aa6f3dd6021e995a9fa49cb590d7bee811223
Yekaterina Shabunin <katka2004@hotmail.com>
2024-11-25 14:49:27 -0500

    week3 assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import './App.css';
+import React, { Component, useState } from 'react';
 import EmployeeForm from './Component/EmployeeForm.js';
+import './App.css';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: []
+    };
+  }
+
+componentDidMount() {
+  const savedEmployees = JSON.parse(localStorage.getItem('employees')) || [];
+  this.setState({ employees: savedEmployees });
+}
+
+
+saveData = () => {
+  localStorage.setItem('employees', JSON.stringify(this.state.employees));
+  };
+
+addEmployee = (employee) => {
+  this.setState(
+    (prevState) => ({
+      employees: [...prevState.employees, employee]
+    }),
+    this.saveData
+  );
+};
+
+render() {
   return (
     <div className="EmployeeForm">
-      <EmployeeForm/>
+      <EmployeeForm addEmployee={this.addEmployee} />
+      <h2>Employee List</h2>
+            <ul>
+          {this.state.employees.map((emp, index) => (
+            <li key={index}>
+              {emp.name} - {emp.email} - {emp.title} - {emp.department}
+            </li>
+          ))}
+            </ul>
     </div>
   );
 }
-
+}
 export default App;
--- src/Component/EmployeeForm.js
+++ src/Component/EmployeeForm.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import '../EmployeeForm.css';
 
-class EmployeeForm extends React.Component {
+class EmployeeForm extends Component{
     constructor(props) {
-        super(props);
-        this.state = {name:'', email: '', title:'', department:''};
+      super(props); 
+      this.state = {name:'', email: '', title:'', department:''};
+      }
     
-    }
+  
 // methods for handling input and form submission
 handleSubmit= (e)=>{
     e.preventDefault();
     console.log(this.state);
+    this.props.addEmployee(this.state);
     this.setState({
         name:'', email: '', title:'', department:''
     });
 };
+
 handleChange= (e)=>{
     const {name,value} = e.target;
     this.setState({
         [name]: value
     });
 };
+ 
 render(){
     return (
+    <div>
         <form className="EmployeeForm" onSubmit={this.handleSubmit}>
             <h1>Add Employee</h1>
             <div>
@@ -43,7 +48,10 @@ render(){
             </div>
             <button type= "submit">Add</button>
         </form>
+        </div>
     );
+
+ }
 }
-}
+
 export default EmployeeForm;
